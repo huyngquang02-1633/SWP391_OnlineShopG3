e96ac1d594273ce97507ccdc87e7bfb2ae77d319
thanh6702 <124339582+thanh6702@users.noreply.github.com>
2023-02-28 00:28:42 +0700

    commit

--- G3_ProjectSwp/src/java/DAL/DBContext.java
+++ G3_ProjectSwp/src/java/DAL/DBContext.java
@@ -27,9 +27,9 @@ public class DBContext {
         //where StudentDBContext is located in dal package, 
         try {
             String user = "sa";
-            String pass = "123";
+            String pass = "123123";
 
-            String url = "jdbc:sqlserver://MSI\\SQLEXPRESS:1433;databaseName=BOOKSHOPB";
+            String url = "jdbc:sqlserver://DESKTOP-H6F27R0\\SQLEXPRESS:1433;databaseName=BOOKSHOP";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException | SQLException ex) {
